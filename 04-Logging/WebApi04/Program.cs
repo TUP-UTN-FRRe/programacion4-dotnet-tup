d06bac82fe856ca74f699af105e33cb037b64ac1
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-09-19 15:00:08 -0300

    [04-Logging] Minimal API. Log por default ILogger<Program>

---
+++ 04-Logging/WebApi04/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddLogging(builder => builder.AddConsole());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (ILogger<Program> logger) =>
+{
+
+    logger.LogInformation("Paso 1) Ingreso al weatherforecast");
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+    logger.LogInformation("Paso 2) se cargo el clima");
+
+    logger.LogError("No se que paso pero hubo un error");
+
+    return forecast;
+});
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
